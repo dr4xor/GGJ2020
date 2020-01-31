de7dc94d5f7120ea8f902182e1b803a672a404d6
Marco <marco@salas.cloud>
2020-01-31 20:06:45 +0100

    Add Player Movement script

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+
+    private Rigidbody2D m_rigidbody;
+
+    [SerializeField] private float movementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if(Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0)
+        {
+            Move(horizontal, vertical);
+        }
+    }
+
+
+    void Move(float h, float v)
+    {
+        m_rigidbody.AddForce(Vector3.right * h * movementSpeed);
     }
 }
