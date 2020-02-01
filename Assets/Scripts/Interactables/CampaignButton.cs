6835303490800e71689031927a0f5b622e9c32d0
Richard Krutisch <richard.dragon.paris@gmail.com>
2020-02-01 22:44:37 +0100

    Add Titlescreen

---
+++ Assets/Scripts/Interactables/CampaignButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CampaignButton : MonoBehaviour
+{
+
+    void Update()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ChangeScene);
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("MovementTest");
+    }
+}
