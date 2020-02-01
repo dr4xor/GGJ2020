730c0419d719e78854d059db1c9f0f8865bddee3
Daniel <boesch.daniel98@yahoo.com>
2020-02-01 22:36:59 +0100

    Add Button Script

---
+++ Assets/Scripts/World/Button.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    bool activated;
+    [SerializeField] private float cooldownSeconds; //-1 if it's not supposed to be reset
+    public UnityEvent uEvent;
+
+    private float startCooldown;
+
+    public void Start()
+    {
+        activated = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (cooldownSeconds == -1)
+        {
+            return;
+        }
+
+        if (activated == false)
+        {
+            if (other.CompareTag("FireProjectile")
+            || other.CompareTag("Boulder")
+            || other.CompareTag("Player"))
+            {
+                uEvent.Invoke();
+            }
+
+            startCooldown = Time.time;
+        }
+
+        StartCoroutine(ButtonCooldown());
+    }
+
+    private IEnumerator ButtonCooldown()
+    {
+        yield return new WaitForSeconds(cooldownSeconds);
+        activated = true;
+
+    }
+}
