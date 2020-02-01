8ac0111dea19060f130824469d71cf0933a7e232
Marco <marco@salas.cloud>
2020-02-01 23:04:41 +0100

    Add Door

---
+++ Assets/Scripts/World/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OpenDoor()
+    {
+        animator.SetBool("doorOpened", true);
+    }
+
+    void CloseDoor()
+    {
+        animator.SetBool("doorOpened", false);
+    }
+}
