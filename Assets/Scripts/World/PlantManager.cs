6487922f02f426255419d1fa70c1fd85d436abb3
Marco <marco@salas.cloud>
2020-02-01 12:26:46 +0100

    Wölkchen added

---
+++ Assets/Scripts/World/PlantManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantManager : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private int growState = 0;
+
+    [SerializeField]
+    private Sprite[] growStateSprites;
+
+    private float totalGrowTime = 0;
+
+    [SerializeField] private float neededGrowTime = 5;
+
+    private bool isInRain = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRain)
+        {
+            if(totalGrowTime < neededGrowTime)
+            {
+                totalGrowTime += Time.deltaTime;
+            }
+
+            growState = Mathf.FloorToInt((totalGrowTime / neededGrowTime) * (growStateSprites.Length - 1));
+
+        }
+
+        spriteRenderer.sprite = growStateSprites[growState];
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if(other.CompareTag("Rain"))
+        {
+            isInRain = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Rain"))
+        {
+            isInRain = false;
+        }
+    }
+}
