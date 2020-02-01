124bbad28f594842f91ac2a3d2e6c43739aa5512
Daniel <boesch.daniel98@yahoo.com>
2020-02-01 15:34:01 +0100

    Add FireSpell

---
+++ Assets/Scripts/Spells/FireSpellManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpellManager : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag != "Player")
+        {
+            Debug.Log("collide (name) : " + collision.collider.gameObject.name);
+            Destroy(gameObject);
+
+            GameObject explosion = GameObject.Instantiate(Resources.Load("Explosion")) as GameObject;
+
+            var contact = collision.contacts[0];
+            Vector3 contactPoint = new Vector3(contact.point.x, contact.point.y, 0);
+
+            explosion.transform.position = contactPoint;
+            Destroy(explosion, 1);
+
+        }
+    }
+}
