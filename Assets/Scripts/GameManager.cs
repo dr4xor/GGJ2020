11a353c5bf712199f5ec7517e8757b62dca09224
Daniel <boesch.daniel98@yahoo.com>
2020-02-01 19:08:40 +0100

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameManager : MonoBehaviour
+{
+
+
+    //player related
+    [SerializeField] GameObject player;
+    private int score;
+    private int mana;
+
+    //map related
+    private int level;
+
+    private readonly int START_SCORE = 0;
+    private readonly int START_MANA = 100;
+    private readonly int START_LEVEL = 1;
+
+
+    private GameManager()
+    {
+        score = START_SCORE;
+        mana = START_MANA;
+        level = START_LEVEL;
+    }
+
+
+    private static GameManager instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+    }
+
+    public void DecreaseMana(int value)
+    {
+        this.mana -= value;
+    }
+
+    public void LoadLevel(int level)
+    {
+        score = START_SCORE;
+        mana = START_MANA;
+        level = START_LEVEL;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
